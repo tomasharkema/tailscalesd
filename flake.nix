6e81b695cfbc05f0d1b8d98b53548679b012affa
tomasharkema <tomas@harkema.io>
2024-07-30 01:57:27 +0200

    update version

--- flake.nix
+++ flake.nix
@@ -1,21 +1,23 @@
 {
+  description = "tailscalesd";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  } @ attrs:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+      perSystem = {pkgs, ...}: {
+        packages = rec {
+          tailscalesd = pkgs.callPackage ./package.nix {};
+          default = tailscalesd;
+        };
       };
-    in {
-      packages = rec {
-        tailscalesd = pkgs.callPackage ./package.nix {};
-        default = tailscalesd;
-      };
-    });
+    };
 }
--- package.nix
+++ package.nix
@@ -4,17 +4,17 @@
 }:
 buildGoModule rec {
   pname = "tailscalesd";
-  version = "0.2.2-1";
+  version = "0.2.2-2";
 
   src = ./.;
 
-  # vendorHash = "sha256-ksJaXSGbN15QMHLR3OAxENiXNS2gsJlYX+C8FLIX0TE=";
   vendorHash = "sha256-UVVMsjrtwgYzZbTizl9/YOznyXqCcxS1waersYPO7zw=";
 
   meta = with lib; {
     description = "tomas";
     homepage = "https://github.com/tomasharkema/nix-config";
     license = licenses.mit;
+    mainProgram = "tailscalesd";
   };
 
   # ASSUME_NO_MOVING_GC_UNSAFE_RISK_IT_WITH = "go1.21";
